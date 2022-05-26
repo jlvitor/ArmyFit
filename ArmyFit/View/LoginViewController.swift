a61893c54c957b79bf62ab1885fd68681a902fc9
Gabriel Baptista Lopes <gabriel_b_lopes@hotmail.com>
2022-05-26 16:31:00 -0300

    IBOUTLETS e IBACTIONS colocados na viewController

--- ArmyFit/View/LoginViewController.swift
+++ ArmyFit/View/LoginViewController.swift
@@ -9,21 +9,24 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func forgotPasswordButton(_ sender: UIButton) {
     }
-    */
-
+    
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func registerButton(_ sender: UIButton) {
+    }
 }
