e062af5dfcdc5bdcc826da33ffb270ca328f5b89
Gabriel Policastro <gabrielpolicastro@outlook.coim>
2022-05-26 19:53:51 -0300

    Layout da tela de cadastro, criaçao das constraints, IBoutlets, IBactions e ViewController

---
+++ ArmyFit/View/RegisterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RegisterViewController.swift
+//  ArmyFit
+//
+//  Created by Gabriel Policastro on 26/05/22.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func registerButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+    }
+    
+
+}
