7c19eaa6a7a00191e1ddd326cfc839867724a8f3
Marcelo Falcão <marcelo_falcaoc@hotmail.com>
2022-06-13 15:26:15 -0300

    Refatoração da TrainingHoursService para consumir direto da API.

---
+++ ArmyFit/Service/TrainingHoursService.swift
@@ -0,0 +1,42 @@
+//
+//  TrainingHoursSerice.swift
+//  ArmyFit
+//
+//  Created by Jean Lucas Vitor on 07/06/22.
+//
+
+import Foundation
+
+class TrainingHoursService {
+    
+    private let baseURL = "https://armyapi.herokuapp.com/hours/2022-06-15"
+    
+    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NTUwOTEzMjcsImV4cCI6MTY4NjY0ODkyNywic3ViIjoiMWMyMjNmNmMtMzBhMS00MzcyLWFjYTItMjc1NWYwNjg3ODhkIn0.0dxYt-RwScqNdF9OAR_BCIiF6JZfWsKiGK1zQMYuesA"
+    
+    func getTrainingHours(completion: @escaping ([TrainingHours]?, Error?) -> Void) {
+        guard let url = URL(string: baseURL) else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer\(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                let training = try JSONDecoder().decode([TrainingHours].self, from: data)
+                DispatchQueue.main.async {
+                    completion(training, nil)
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+            }
+        }
+        task.resume()
+    }
+}
+
+
