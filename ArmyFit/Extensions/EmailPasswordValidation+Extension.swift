8c836f53d15fad604f6f32673d8878d650deecaa
Gabriel Baptista Lopes <gabriel_b_lopes@hotmail.com>
2022-06-18 01:29:18 -0300

    Criada as extensions de validacao de textfields para email e password e implementada na tela de registerViewController. Criados os toggle butons na tela de register

---
+++ ArmyFit/Extensions/EmailPasswordValidation+Extension.swift
@@ -0,0 +1,36 @@
+//
+//  EmailPasswordValidation+Extension.swift
+//  ArmyFit
+//
+//  Created by Gabriel Lopes on 18/06/22.
+//
+
+import Foundation
+
+
+
+//MARK: - Parâmetros de validação dos textfields com caractéres corretos
+extension String {
+    
+    func isValidEmail(email: String) -> Bool {
+        
+        //https://emailregex.com
+        
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        let emailResult = emailTest.evaluate(with: email)
+        return emailResult
+    }
+    
+    func isValidPassword(password: String) -> Bool {
+        let passwordRegEx = "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{6,10}"
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
+        let passwordResult = passwordTest.evaluate(with: password)
+        return passwordResult
+        
+        // Senha com letras, 1 caractere especial, uma letra maiuscula e números opcionais
+        // Mínimo 6 e máx 10 caracteres permitidos (Pode trocar {6,16}
+        // email padrao xxxxx@yyyyy.com
+    }
+    
+}
--- ArmyFit/View/RegisterViewController.swift
+++ ArmyFit/View/RegisterViewController.swift
@@ -15,6 +15,11 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var confirmPasswordTextField: UITextField!
     @IBOutlet weak var loginScreen: UIStackView!
     
+    private var iconClick =  false
+    private let imageIcon = UIImageView()
+    private var iconClick2 =  false
+    private let imageIcon2 = UIImageView()
+    
     private let viewModel: RegisterViewModel = RegisterViewModel()
     
     override func viewDidLoad() {
@@ -22,13 +27,140 @@ class RegisterViewController: UIViewController {
         hideKeyboardWhenTappedAround()
         configGestureRecognizer()
         viewModel.delegate = self
+        configureContentViewPassword()
+        configureImageIcon()
+        configureContentViewConfirmPassword()
+        configureImageIcon2()
     }
     
+    //MARK: - Toggle textfield password
+        private func configureContentViewPassword() {
+             let contentView = UIView()
+             contentView.addSubview(imageIcon)
+             
+             contentView.frame = CGRect(x: 100,
+                                        y: 100,
+                                        width: UIImage(systemName: "eye.slash")!.size.width,
+                                        height: UIImage(systemName: "eye.slash")!.size.width
+             )
+             
+             imageIcon.frame = CGRect(x: -10,
+                                      y: 0,
+                                      width: UIImage(systemName: "eye.slash")!.size.width,
+                                      height: UIImage(systemName: "eye.slash")!.size.height
+             )
+             
+             passwordTextField.rightView = contentView
+             passwordTextField.rightViewMode = .always
+             
+             let tapGestureRecognizer = UITapGestureRecognizer(
+                 target: self,
+                 action: #selector(imageTapped(tapGestureRecognizer:)))
+             imageIcon.isUserInteractionEnabled = true
+             imageIcon.addGestureRecognizer(tapGestureRecognizer)
+         }
+         private func configureImageIcon() {
+             imageIcon.image = UIImage(systemName: "eye.slash")
+             imageIcon.tintColor = .black
+         }
+        @objc private func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+             
+             let tappedImage = tapGestureRecognizer.view as! UIImageView
+             
+             if iconClick {
+                 iconClick = false
+                 tappedImage.image = UIImage(systemName: "eye")
+                 passwordTextField.isSecureTextEntry = false
+             }
+             else {
+                 iconClick = true
+                 tappedImage.image = UIImage(systemName: "eye.slash")
+                 passwordTextField.isSecureTextEntry = true
+                 
+             }
+         }
+        
+    //MARK: - Toggle textfield ConfirmPassword
+        private func configureContentViewConfirmPassword() {
+             let contentView = UIView()
+             contentView.addSubview(imageIcon2)
+             
+             contentView.frame = CGRect(x: 100,
+                                        y: 100,
+                                        width: UIImage(systemName: "eye.slash")!.size.width,
+                                        height: UIImage(systemName: "eye.slash")!.size.width
+             )
+             
+             imageIcon2.frame = CGRect(x: -10,
+                                      y: 0,
+                                      width: UIImage(systemName: "eye.slash")!.size.width,
+                                      height: UIImage(systemName: "eye.slash")!.size.height
+             )
+             
+            confirmPasswordTextField.rightView = contentView
+            confirmPasswordTextField.rightViewMode = .always
+             
+             let tapGestureRecognizer = UITapGestureRecognizer(
+                 target: self,
+                 action: #selector(imageTappedConfirm(tapGestureRecognizer:)))
+             imageIcon2.isUserInteractionEnabled = true
+             imageIcon2.addGestureRecognizer(tapGestureRecognizer)
+         }
+         private func configureImageIcon2() {
+             imageIcon2.image = UIImage(systemName: "eye.slash")
+             imageIcon2.tintColor = .black
+         }
+        @objc private func imageTappedConfirm(tapGestureRecognizer: UITapGestureRecognizer) {
+             
+             let tappedImage = tapGestureRecognizer.view as! UIImageView
+             
+             if iconClick2 {
+                 iconClick2 = false
+                 tappedImage.image = UIImage(systemName: "eye")
+                 confirmPasswordTextField.isSecureTextEntry = false
+             }
+             else {
+                 iconClick2 = true
+                 tappedImage.image = UIImage(systemName: "eye.slash")
+                 confirmPasswordTextField.isSecureTextEntry = true
+                 
+             }
+         }
+  
+        
     @IBAction func registerButton(_ sender: UIButton) {
         viewModel.makeRegisterRequest(
             nameTextField.text,
             emailTextField.text,
             passwordTextField.text)
+//MARK: Autenticação dos textfields
+        if let name = nameTextField.text,
+           let email = emailTextField.text,
+           let password = passwordTextField.text,
+           let confirmPassword = confirmPasswordTextField.text {
+            
+            if name == "" || email == "" || password == "" {
+                ErrorAlertsTextfield().alertTextField(
+                    vc: self,
+                    title: "Erro",
+                    message: "Por favor preencha todos os campos")
+            } else if password != confirmPassword {
+                ErrorAlertsTextfield().alertTextField(
+                    vc: self,
+                    title: "Erro",
+                    message: "Por favor digite a mesma senha nos dois campos")
+            } else {
+                if !email.isValidEmail(email: email) {
+                    ErrorAlertsTextfield().alertTextField(vc: self, title: "Opa!", message: "Por favor coloque um email válido")
+                }
+                 else if !password.isValidPassword(password: password) {
+                     ErrorAlertsTextfield().alertTextField(vc: self, title: "Opa!", message: "Por favor coloque uma senha válida")
+                    
+                 } else {
+                  performSegue(withIdentifier: "backToLoginScreen", sender: self)
+                    }
+                }
+        }
     }
     
     private func showAlert() {
