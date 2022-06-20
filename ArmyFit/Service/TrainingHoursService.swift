3fe0c4505d5adbcb3497915a0250eb15df7955d0
Marcelo Falcão <marcelo_falcaoc@hotmail.com>
2022-06-20 16:04:36 -0300

    Ajuste da tela do treino, adicionar e remover na lista de usuários do treino selecionado.

--- ArmyFit/Service/TrainingHoursService.swift
+++ ArmyFit/Service/TrainingHoursService.swift
@@ -13,7 +13,7 @@ class TrainingHoursService {
     private let baseUrl = "https://armyapi.herokuapp.com"
     private let keychain: KeychainSwift = .init()
     
-    //MARK: - Pega os treinos dos dias no mes atual
+    // Pega todos os treinos dos dias no mes atual
     func getAllTrainingHours(_ date: String, completion: @escaping ([TrainingHours]?, Error?) -> Void) {
         guard let url = URL(string: "\(baseUrl)/hours/\(date)") else { return }
         
@@ -25,42 +25,61 @@ class TrainingHoursService {
             guard let data = data, error == nil else { return }
             
             do {
-                let training = try JSONDecoder().decode([TrainingHours].self, from: data)
+                let trainings = try JSONDecoder().decode([TrainingHours].self, from: data)
                 DispatchQueue.main.async {
-                    completion(training, nil)
+                    completion(trainings, nil)
                 }
             } catch {
-                DispatchQueue.main.async {
-                    completion(nil, error)
-                }
+                completion(nil, error)
             }
         }
         task.resume()
     }
     
-    func addUserOnTraining(_ userId: String, _ trainingHoursId: String, completion: @escaping (TrainingUser?, Error?) -> Void) {
+    // Pega um treino em especifico
+    func getTrainingHours(_ trainingId: String, completion: @escaping (TrainingHours?, Error?) -> Void) {
+        guard let url = URL(string: "\(baseUrl)/hours/training/\(trainingId)") else { return }
         
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Bearer \(keychain.get("token"))", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                let training = try JSONDecoder().decode(TrainingHours.self, from: data)
+                DispatchQueue.main.async {
+                    completion(training, nil)
+                }
+            } catch {
+                completion(nil, error)
+            }
+        }
+        task.resume()
+    }
+    
+    // Inscreve o usuario no treino
+    func addUserOnTraining(_ trainingHoursId: String, _ userId: String, completion: @escaping (TrainingUser?, Error?) -> Void) {
         guard let url = URL(string: "\(baseUrl)/trainingusers") else { return }
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue("Bearer \(keychain.get("token"))", forHTTPHeaderField: "Authorization")
-        
         let body: [String: String] = [
             "trainingHoursId": trainingHoursId,
-            "userId": userId
+            "user_id": userId
         ]
-        
         request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
         
         let task = URLSession.shared.dataTask(with: request) { data, _, error in
             guard let data = data, error == nil else { return }
             
             do {
-                let training = try JSONDecoder().decode(TrainingUser.self, from: data)
+                let trainingRegister = try JSONDecoder().decode(TrainingUser.self, from: data)
                 DispatchQueue.main.async {
-                    completion(training, nil)
+                    completion(trainingRegister, nil)
                 }
             } catch {
                 completion(nil, error)
