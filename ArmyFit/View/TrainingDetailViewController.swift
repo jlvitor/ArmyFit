c2bdec321d8450a89fa629ab8c302b2246b4c2da
Gabriel Policastro <gabrielpolicastro@outlook.coim>
2022-06-21 17:06:18 -0300

    Criacao da tela de treino e detalhe do treino

---
+++ ArmyFit/View/TrainingDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TrainingViewController.swift
+//  ArmyFit
+//
+//  Created by Gabriel Policastro on 08/06/22.
+//
+
+import UIKit
+
+class TrainingDetailViewController: UIViewController {
+    @IBOutlet weak var warningLabel: UILabel!
+    @IBOutlet weak var trainingLabel: UILabel!
+    
+    
+    private var viewModel: TrainingViewModel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
