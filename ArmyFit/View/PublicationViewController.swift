2c2f49ad09be1942bae79c5669ad72d3fe70248d
Gabriel Baptista Lopes <gabriel_b_lopes@hotmail.com>
2022-06-24 16:34:03 -0300

    Criada a tela de postagem do usuário e sua respectiva viewController

---
+++ ArmyFit/View/PublicationViewController.swift
@@ -0,0 +1,42 @@
+//
+//  PublicationViewController.swift
+//  ArmyFit
+//
+//  Created by Gabriel Lopes on 24/06/22.
+//
+
+import UIKit
+
+class PublicationViewController: UIViewController {
+    
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var publicationUserTextView: UITextView!
+    
+    @IBOutlet weak var publicationUserPoster: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        publicationUserTextView.text = "No que você está pensando?"
+        publicationUserTextView.textColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 0.5)
+        
+        
+       
+    }
+    //MARK: - Configuração do textView
+    func textViewDidBeginEditing(_ publicationUserTextView: UITextView) {
+        if publicationUserTextView.textColor == UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 0.5) {
+            publicationUserTextView.text = nil
+            publicationUserTextView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ publicationUserTextView: UITextView) {
+        if publicationUserTextView.text.isEmpty {
+            publicationUserTextView.text = "No que você está pensando?"
+            publicationUserTextView.textColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 0.5)
+        }
+    }
+   
+
+}
