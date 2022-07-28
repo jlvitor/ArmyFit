86b4dc8bbac42b99eb3b3c75a208f925bdc9d2fa
Gabriel Baptista Lopes <gabriel_b_lopes@hotmail.com>
2022-07-28 15:04:02 -0300

    Ajustada a CommentViewModel

--- ArmyFit/ViewModels/Comments/CommentsViewModel.swift
+++ ArmyFit/ViewModels/Comments/CommentsViewModel.swift
@@ -9,17 +9,35 @@ import Foundation
 import UIKit
 
 protocol CommentsViewModelDelegate {
-    func reloadTableView()
+    func reloadData()
 }
 
 class CommentsViewModel {
     
+    private let service: PostService = .init()
     var delegate: CommentsViewModelDelegate?
+    var commentList: [CommentPost] = []
     
     func getNumberOfComments() -> Int {
-        return 5
+        return commentList.count
     }
     
+//    var getUserImage: String {
+//        UserDefaults.getValue(key: UserDefaults.Keys.userPhoto) as? String ?? "profile"
+//    }
+    
+//    var getUserName: String {
+//            UserDefaults.getValue(key: UserDefaults.Keys.userName) as? String ?? "profile"
+//        }
+    
+    
+//    func getComments() {
+//        service.commentPost(feedId: CommentPost.id, comment: CommentPost.comment) { <#[CommentPost]?#>, error in
+//            guard let comment = comment else { return }
+//            self.commentList = comment
+//            self.delegate?.reloadData()
+//        }
+//
     
 }
 
